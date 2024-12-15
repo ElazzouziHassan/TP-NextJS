b1c4c472e70bf547a77886369a1f34ac3c817467
wizardy <wizardhassan99@gmail.com>
2024-12-15 14:48:00 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-// next.config.js
-module.exports = {
+const nextConfig = {
   eslint: {
-    // Optionally ignore ESLint errors during build (useful for production)
-    ignoreDuringBuilds: true,
-
-    // Disable specific ESLint rules
-    rules: {
-      'no-unused-vars': 'off', // Disables the 'no-unused-vars' rule
-      'react/prop-types': 'off', // Disables the 'react/prop-types' rule
-      'no-console': 'off', // Disables the 'no-console' rule
-    },
+    ignoreDuringBuilds: true, // Disable ESLint during build process
   },
 };
 
